d21418093a7d9a3272a32563d2d472d2f562b55a
danie <danielrgc97@gmail.com>
2020-08-27 11:25:51 +0200

    Correcto uso de Storage conseguido

--- src/app/main-timers/cajas.service.ts
+++ src/app/main-timers/cajas.service.ts
@@ -1,27 +1,47 @@
 import { Injectable } from '@angular/core';
 import { Caja } from './caja.model';
 
+import { Plugins} from '@capacitor/core';
+const { Storage } = Plugins;
+
 @Injectable({
   providedIn: 'root'
 })
 export class CajasService {
+  cajas: Caja[] = [];
+  json = [];
 
-  private cajas: Caja[] = [
-    {
-      id: '1',
-      nombre: 'abs',
-      timerValue: '10'
-    },
-    {
-      id: '2',
-      nombre: 'pushups',
-      timerValue: '5'
-    }
-  ];
-
-  constructor() { }
+  constructor() {
+    // this.json = [
+    //   {id: '1', nombre: 'jon', timerValue: '10'},
+    //   {id: '2', nombre: 'miguel', timerValue: '10'},
+    //   {id: '3', nombre: 'kike', timerValue: '10'}
+    // ];
+  }
 
   getAllCajas(){
+    this.getObjects();
     return [...this.cajas];
   }
+
+  async getObjects() {
+    const j = await Storage.get({ key: '8Timer' });
+    this.json = JSON.parse(j.value);
+
+    for ( let i = 0 ; i < this.json.length ; i++){
+      this.cajas[i] = this.json[i];
+    }
+  }
+
+  async setObjects() {
+    console.log(this.json);
+    await Storage.set({key: '8Timer', value: JSON.stringify(this.json)
+    });
+  }
+
+
+  addCaja(id: string, nombre: string, timerValue: string){
+    this.cajas.push({ id, nombre, timerValue});
+  }
+
 }
--- src/app/main-timers/main-timers.page.ts
+++ src/app/main-timers/main-timers.page.ts
@@ -4,8 +4,10 @@ import { CajasService } from './cajas.service';
 
 import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
 
-const { Filesystem } = Plugins;
 const { Storage } = Plugins;
+
+const { Filesystem } = Plugins;
+
 
 @Component({
   selector: 'app-main-timers',
@@ -14,46 +16,21 @@ const { Storage } = Plugins;
 })
 export class MainTimersPage implements OnInit {
   cajas: Caja[];
-  jsonString: string;
+  json = [];
 
   constructor( private cajasService: CajasService) {}
 
   ngOnInit() {
-    this.cajas = this.cajasService.getAllCajas();
+    this.cajas = this.cajasService
   }
 
 
-  testing(){
-    console.log();
+  a(){
+    this.cajasService.setObjects();
   }
 
-  // async fileWrite() {
-  //   try {
-  //     const result = await Filesystem.writeFile({
-  //       path: '/secrets/text.txt',
-  //       data: "This is a test",
-  //       directory: FilesystemDirectory.Data,
-  //       recursive: true,
-  //       encoding: FilesystemEncoding.UTF8
-  //     })
-  //     console.log('Wrote file', result);
-  //   } catch(e) {
-  //     console.error('Unable to write file', e);
-  //   }
-  // }
-
-  // async setObject() {
-  //   await Storage.set({
-  //     key: '8Timer',
-  //     value: JSON.stringify(
-  //     )
-  //   });
-  // }
-
-  // // JSON "get" example
-  // async getObject() {
-  //   const ret = await Storage.get({ key: '8Timer' });
-  //   const user = JSON.parse(ret.value);
-  // }
+  b(){
+    this.cajasService.getObjects();
+  }
 
 }
